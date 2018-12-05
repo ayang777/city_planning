d87b28ffc18cefc8dcda159be4c20eb44ef3f256
Ingrid Fan <ingridyfan@gmail.com>
2018-12-04 17:25:07 -0800

    added sps

--- sars_data.py
+++ sars_data.py
@@ -10,7 +10,6 @@ df = pd.read_csv('street_intersections.csv')
 #neg and pos of lat and long
 grouped = df.groupby(by="ST_NAME")
 
-
 sp = {}
 
 def make_coords_dict(group):
@@ -41,7 +40,26 @@ def distance(p1, p2):
 	return dist  
 
 
-def main():
+def check_sides(p1, p2, p3, cur_sps):
+	x1 = p1[0]
+	y1 = p1[1]
+	x2 = p2[0]
+	y2 = p2[1]
+	x3 = p3[0]
+	y3 = p3[0]
+
+	same_y = 0
+	if (y3 - y1 > 0 and y2 - y1 > 0) or (y3 - y1 < 0 and y2 - y1 < 0):
+		same_y = 1
+	same_x = 0
+	if (x3 - x1 > 0 and x2 - x1 > 0) or (x3 - x1 < 0 and x2 - x1 < 0):
+		same_x = 1
+
+	if same_y and same_x:
+		cur_sps.pop()
+
+
+def find_sp():
 	for name, group in grouped:
 
 
@@ -57,24 +75,30 @@ def main():
 
 			sort_dists = sorted(dists.items(), key=lambda tup: tup[1])
 
-			# print coords_dict[sort_dists[0][0]]
-			# print coords_dict[sort_dists[1][0]]
+
 			cur_sps = []
 			if len(sort_dists) == 1:
-				cur_sps.append(sort_dists[0])
+				cur_sps.append(sort_dists[0][0])
 
 			elif len(sort_dists) > 1:
-				cur_sps = [sort_dists[0], sort_dists[1]]
+				cur_sps = [sort_dists[0][0], sort_dists[1][0]]
+				check_sides(coord, coords_dict[sort_dists[0][0]], coords_dict[sort_dists[1][0]], cur_sps)
 
 			if cnn in sp:
 				cur_list = sp[cnn]
 				sp[cnn] = list(set(cur_list + cur_sps))
-				#print sp[cnn]
+
 			else:
 				sp[cnn] = cur_sps
-			#print sp[cnn]
+
+find_sp()
+new_df = []
+for s, sps in sp.iteritems():
+	for sp in sps:
+		new_df.append([s, sp])
+
+ndf = pd.DataFrame(new_df, columns = ['s', 'sp'])
+ndf.to_csv("s_to_sp.csv")
 
 
-	print len(sp)
 
-main()
